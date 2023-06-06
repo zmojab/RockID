ecb84f67de06926fb55b1c3fb326462e82cdbb87
Syed Usman <64466923+UZIIMAN@users.noreply.github.com>
2023-06-06 11:58:54 -0400

    fixed syntax error

--- rockid/lib/services/auth_service.dart
+++ rockid/lib/services/auth_service.dart
@@ -3,19 +3,19 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   //google sign in
-  signInWithGoogle() async (
+  signInWithGoogle() async {
     //begin interactive sign in
     final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
 
-    //obtain aut details from request 
+    //obtain aut details from request
     final GoogleSignInAuthentication gAuth = await gUser!.authentication;
 
-    //create a new credential for user 
-    final credential = GoogleAuthProvider.credential {
+    //create a new credential for user
+    final credential = GoogleAuthProvider.credential(
       accessToken: gAuth.accessToken,
       idToken: gAuth.idToken,
-    }
+    );
     //sign in
     return await FirebaseAuth.instance.signInWithCredential(credential);
-  )
+  }
 }
