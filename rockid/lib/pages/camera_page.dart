075edaabd8e456064b6c804db98c6511abd7572f
jim-wallace <j.wallace27183@gmail.com>
2023-06-21 02:09:50 -0400

    Updated camera version to 10.5 to fix error, added button to take photo on tap and added print statement to locate file for now. Also added nav bar to return to home page and to go to profile page for temporary navigation.

--- rockid/lib/pages/camera_page.dart
+++ rockid/lib/pages/camera_page.dart
@@ -1,5 +1,7 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:rockid/pages/Home_page.dart';
+import 'package:rockid/pages/profile_page.dart';
 
 class CameraScreen extends StatefulWidget {
   const CameraScreen({Key? key}) : super(key: key);
@@ -11,6 +13,9 @@ class CameraScreen extends StatefulWidget {
 class _CameraScreenState extends State<CameraScreen> {
   late List<CameraDescription> cameras;
   CameraController? cameraController;
+
+  int _selectedIndex = 0;
+  final List<Widget> _pages = [HomePage(), ProfilePage()];
 
   @override
   void initState() {
@@ -51,12 +56,73 @@ class _CameraScreenState extends State<CameraScreen> {
         body: Stack(
           children: [
             CameraPreview(cameraController!),
+            GestureDetector(
+              onTap: () {
+                cameraController!.takePicture().then((XFile? file) {
+                  if(mounted) {
+                    if(file != null) {
+                      print("Picture saved to ${file.path}");
+                    }
+                  }
+                });
+              },
+              child: button(Icons.camera_alt_outlined, Alignment.bottomCenter), 
+            )
           ],
-        )
-
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        onTap: (index) {
+          // Update the state and rebuild the widget
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => _pages[index]),
+          );
+        },
+      ),
       );
     } else {
       return const SizedBox();
     }
   }
+}
+
+Widget button(IconData icon, Alignment alignment) {
+  return Align(
+    alignment: alignment,
+    child: Container(
+      margin: const EdgeInsets.only(
+          left: 20,
+          bottom: 20,
+      ),
+      height: 50,
+      width: 50,
+      decoration: const BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black26,
+            offset: Offset(2, 2),
+            blurRadius: 10,
+          ),
+        ],
+      ),
+      child: Center(
+        child: Icon(
+          icon,
+          color: Colors.black54,
+        ),
+      ),
+    ),
+  );
 }
