e936be5615ca63776cb5389d4af007a531d66871
Syed Usman <64466923+UZIIMAN@users.noreply.github.com>
2023-07-07 17:17:16 -0400

    comments added

--- rockid/lib/pages/maps.dart
+++ rockid/lib/pages/maps.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+//added google maps package - SU
+
+//location of detroit for the pin that will be using this location to populate infomration on rock, this location will change to current location - SU
 const LatLng currentLocation = LatLng(42.3314, -83.0458);
 
+
+//page initiation 
 class MapsPage extends StatefulWidget {
   @override
   _MapsPageState createState() => _MapsPageState();
@@ -12,14 +17,16 @@ class _MapsPageState extends State<MapsPage> {
   late GoogleMapController mapController;
   Set<Marker> markers = Set<Marker>();
 
+
+  
   @override
   void initState() {
     super.initState();
     markers.add(
       Marker(
-        markerId: MarkerId('locationMarker'),
+        markerId: MarkerId('locationMarker'), //initiate marker which will be later populated by the lat and long of found rocks - SU
         position: currentLocation,
-        onTap: () {
+        onTap: () { //marker info which will be given by rock information page, this will also show user who found the rock (if stated true) - SU
           showDialog(
             context: context,
             builder: (context) => AlertDialog(
@@ -40,6 +47,7 @@ class _MapsPageState extends State<MapsPage> {
     );
   }
 
+  //map initial state with control - SU
   @override
   Widget build(BuildContext context) {
     return Scaffold(
