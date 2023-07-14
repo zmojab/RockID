78bbad433289cec3abb58f091ed6118292a1ca1f
jim-wallace <j.wallace27183@gmail.com>
2023-07-14 02:02:02 -0400

    added changes to edit profile and rocks found page

---
+++ rockid/lib/data/users.dart
@@ -0,0 +1,98 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+//users CRUD class for operations on user collection JW
+//methods extracted from edit_profile and originally introduced by AW
+
+class UserCRUD {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  Future<String?> getUserDocID(String uid) async {
+    try {
+      var docID = await _firestore
+          .collection("users")
+          .where("UID", isEqualTo: uid)
+          .get();
+      if (docID.docs.isNotEmpty) {
+        return docID.docs[0].id;
+      }
+    } catch (error) {
+      print('Error getting user doc ID: $error');
+    }
+    return null;
+  }
+
+  Future<DocumentSnapshot> getUserProfile(String? docID) async {
+    if (docID != null) {
+      return _firestore.collection('users').doc(docID).get();
+    }
+    throw Exception('User doc ID is null');
+  }
+
+  Future<QuerySnapshot> getUserByUsername(String username) async {
+    return _firestore
+        .collection("users")
+        .where("username", isEqualTo: username)
+        .limit(1)
+        .get();
+  }
+
+  Future<void> updateUserOccupation(String? docID, String occupation) async {
+    if (docID != null) {
+      await _firestore.collection("users").doc(docID).update({
+        'occupation': occupation,
+      });
+    } else {
+      throw Exception('User doc ID is null');
+    }
+  }
+
+ Future<void> updateUserProfileUrl(String? docID, String? profileUrl) async {
+    if (docID != null) {
+      await _firestore.collection("users").doc(docID).update({
+        'user_profile_url': profileUrl,
+      });
+    } else {
+      throw Exception('User doc ID is null');
+    }
+  }
+  
+  Future<void> updateUserUsername(String? docID, String username) async {
+    if (docID != null) {
+      await _firestore.collection("users").doc(docID).update({
+        'username': username,
+      });
+    } else {
+      throw Exception('User doc ID is null');
+    }
+  }
+
+  Future<void> updateUserLocation(String? docID, String location) async {
+    if (docID != null) {
+      await _firestore.collection("users").doc(docID).update({
+        'location': location,
+      });
+    } else {
+      throw Exception('User doc ID is null');
+    }
+  }
+
+  Future<void> updateUserBio(String? docID, String bio) async {
+    if (docID != null) {
+      await _firestore.collection("users").doc(docID).update({
+        'bio': bio,
+      });
+    } else {
+      throw Exception('User doc ID is null');
+    }
+  }
+
+Future<void> updateUserProfileStatus(String? docID, bool isPublic) async {
+    if (docID != null) {
+      await _firestore.collection("users").doc(docID).update({
+        'public': isPublic,
+      });
+    } else {
+      throw Exception('User doc ID is null');
+    }
+  }
+
+}
